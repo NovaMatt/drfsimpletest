b7f7444812bdd803c0d524d4e8759ea340115f9f
Matias Benitez <matiasuzumy@gmail.com>
2023-01-19 15:26:52 -0300

    first commit

---
+++ projects/urls.py
@@ -0,0 +1,12 @@
+from rest_framework import routers
+from  .api import ProjectViewSet
+
+
+
+router=routers.DefaultRouter()
+router.register('api/projects', ProjectViewSet,'projects')
+
+
+
+urlpatterns=router.urls
+
